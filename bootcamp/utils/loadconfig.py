75021432c67ffdecefdb4e15ab33748104c024fe
davismathew <davis.kuriath@gmail.com>
2016-11-16 03:41:44 +0530

    fixing bugs

--- bootcamp/utils/loadconfig.py
+++ bootcamp/utils/loadconfig.py
@@ -9,15 +9,15 @@ def readconf():
     return config
 
 
-def project_path(type):
-    config = readconf()
-    if type == 'project':
-        path = config.get('paths', 'project_path')
-    elif type == 'play':
-        path = config.get('paths', 'project_path')
-    elif type == 'resultout':
-        path = config.get('paths', 'result_path')
-    return os.listdir(path)
+# def project_path(type):
+#     config = readconf()
+#     if type == 'project':
+#         path = config.get('paths', 'project_path')
+#     elif type == 'play':
+#         path = config.get('paths', 'project_path')
+#     elif type == 'resultout':
+#         path = config.get('paths', 'result_path')
+#     return os.listdir(path)
 
 def get_vars(type):
     config = readconf()
